b79f5dba4fc2ec307eb7016b20456d217321fcda
Akkyie <akio@prioris.org>
2015-01-29 22:07:38 +0900

    Initial Commit

---
+++ AKPickerView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AKPickerView
+//
+//  Created by Akio Yasui on 1/29/15.
+//  Copyright (c) 2015 Akkyie Y. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
